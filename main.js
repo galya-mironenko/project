2309898c0ad0407758f93788a097af89ebd7f256
Galina <galinka.mironenko.96@gmail.com>
2020-07-02 15:36:53 +0300

    ovies rating

--- main.js
+++ main.js
@@ -8,10 +8,33 @@ const  personalMovieDB = {
     privat: false
 };
 
-const film = prompt("Один из последних просмотренных фильмов?", " "); 
-const filmsGrate = +prompt("На сколько оцените его?", " "); 
+for (let numberQuestion = 0; numberQuestion < 2; numberQuestion++) {
+    const film = prompt("Один из последних просмотренных фильмов?", " "); 
+    const filmsGrate = +prompt("На сколько оцените его?", " "); 
+    
+if (film != null && filmsGrate != null && film != "" && filmsGrate!= "" && film.length < 50) {
+    personalMovieDB.movies[film] = filmsGrate;
+    console.log("Готово!");
+} else {
+    console.log("Ошибка в данных, введите значения корректно!");
+    numberQuestion--;
+    }
+}
 
-personalMovieDB.movies[film] = filmsGrate;
+if(personalMovieDB.count <= 10){
+    console.log("Просмотрено мало фильмов");
+}else if(personalMovieDB.count > 10 && personalMovieDB.count <= 30 ){
+    console.log("Вы классический зритель");
+}else if(personalMovieDB.count >= 30){
+    console.log("Вы киноман");
+}else{
+    console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
+
+
+
+
+
 
